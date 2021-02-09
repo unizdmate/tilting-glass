afd487a222e0bc2afc86210289c6b28f7f18730b
Mate Krezic <matekrezic@gmail.com>
2021-02-09 21:40:34 +0100

    added tillt to title

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import styles from "../styles/Home.module.css";
 import CardList from "./../components/CardList";
+import Tilt from "react-parallax-tilt";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h2>Try and hover over the cards!</h2>
+      <Tilt>
+        <h2>Try and hover over the cards or this title!</h2>
+      </Tilt>
       <CardList />
     </div>
   );
